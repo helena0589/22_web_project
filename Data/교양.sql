19791ef219eae130ef91510b37e5ee303cb876e8
조희우 <84004687+helena0589@users.noreply.github.com>
2022-11-23 20:16:34 +0900

    Data : 기계공학부, 교양
    - 기계공학부 교재 데이터
    - 교양학과 교재 데이터

---
+++ Data/교양.sql
@@ -0,0 +1,23 @@
+/*
+-- Query: SELECT * FROM major.교양
+LIMIT 0, 1000
+
+-- Date: 2022-11-23 16:54
+*/
+INSERT INTO `` (`강의`,`교수`,`교재명`,`가격`,`책 주소`) VALUES ('DFS (동양건강체육)','강상욱','건강 체육의 이론과 실제','12,000','http://image.yes24.com/Goods/37127170/L');
+INSERT INTO `` (`강의`,`교수`,`교재명`,`가격`,`책 주소`) VALUES ('기초 일본어 회화','최지수','감바레 일본어 step 1	','14,000','http://image.yes24.com/Goods/8151542/L');
+INSERT INTO `` (`강의`,`교수`,`교재명`,`가격`,`책 주소`) VALUES ('대화의 기법','양영화, 노지영','입은 하나 귀는 둘	','20,000','http://image.yes24.com/Goods/74596794/L');
+INSERT INTO `` (`강의`,`교수`,`교재명`,`가격`,`책 주소`) VALUES ('레크레이션과 여가생활','강상욱, 강민욱','현대여가론 (개정판)','26,000','http://image.yes24.com/Goods/251687/L');
+INSERT INTO `` (`강의`,`교수`,`교재명`,`가격`,`책 주소`) VALUES ('백세시대의 부자학','최민서','백세시대의 부자학','26,000','https://url.kr/fot8j4');
+INSERT INTO `` (`강의`,`교수`,`교재명`,`가격`,`책 주소`) VALUES ('생활 중국어 회화','박현곤','북경대학 한어구어1','13,500','http://image.yes24.com/Goods/4767785/L');
+INSERT INTO `` (`강의`,`교수`,`교재명`,`가격`,`책 주소`) VALUES ('실무말하기와 글쓰기 (의사소통능력)','유미향, 양정화, 김현정','의사소통능력','20,000','http://image.yes24.com/Goods/61406498/L');
+INSERT INTO `` (`강의`,`교수`,`교재명`,`가격`,`책 주소`) VALUES ('실천리더십','정지현, 한아름','리더십론','20,000','http://image.yes24.com/Goods/33504070/L');
+INSERT INTO `` (`강의`,`교수`,`교재명`,`가격`,`책 주소`) VALUES ('영어듣기','김옥연, 전혜선','YBM스타트 토익 LC','13,000','http://image.yes24.com/Goods/50310515/L');
+INSERT INTO `` (`강의`,`교수`,`교재명`,`가격`,`책 주소`) VALUES ('영어회화','Stephen','NEW English Conversation Arts 1','14,000','http://image.yes24.com/Goods/89873002/L');
+INSERT INTO `` (`강의`,`교수`,`교재명`,`가격`,`책 주소`) VALUES ('영화와 음악','김상혜, 정현혜','아는 음악 몰랐던 영화','20,000','http://image.yes24.com/Goods/36120224/L');
+INSERT INTO `` (`강의`,`교수`,`교재명`,`가격`,`책 주소`) VALUES ('예술과 인성','김유나','예술은 내 안에서 완성된다','18,000','http://image.yes24.com/Goods/74598207/L');
+INSERT INTO `` (`강의`,`교수`,`교재명`,`가격`,`책 주소`) VALUES ('진로설계와 실천인성','우영지','대학생을 위한 진로코칭 : 전략과 실제','14,000','http://image.yes24.com/Goods/35151383/L');
+INSERT INTO `` (`강의`,`교수`,`교재명`,`가격`,`책 주소`) VALUES ('창의력과 문제해결 (문제해결능력)','유미향, 양정화','창의력 문제해결의 힘','12,000','http://image.yes24.com/Goods/3042191/L');
+INSERT INTO `` (`강의`,`교수`,`교재명`,`가격`,`책 주소`) VALUES ('초급 영작문 -1','도해자','차근차근 스피킹이 되는 영작문','13,000','http://image.yes24.com/Goods/23124237/L');
+INSERT INTO `` (`강의`,`교수`,`교재명`,`가격`,`책 주소`) VALUES ('초급영작문-2','김옥연, 윤상미','Longman Academic Writing Series 1','22,000','http://image.yes24.com/Goods/30133481/L');
+INSERT INTO `` (`강의`,`교수`,`교재명`,`가격`,`책 주소`) VALUES ('취업 영어','전혜선, 윤상미, 도해자','YBM스타트 토익 RC','13,000','http://image.yes24.com/Goods/45878047/L');
